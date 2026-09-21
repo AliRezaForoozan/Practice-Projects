37855f0d332dca66e4cf289d5fe4f0f2ec94ad56
alirf <designerafg@gmail.com>
2026-09-21 17:01:16 +0330

    Initial Django Test Project

---
+++ MYJAN_PR1/Home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from . models import person
+
+def home(request):
+
+    allField = person.objects.all()
+    frst = person.objects.first()
+    dic_persons = {"First_Name":"AliR","Last_Name":"Forouzan","allfield":allField,"first":frst}
+    return render(request,'index.html',context=dic_persons)
+
+# Create your views here.
