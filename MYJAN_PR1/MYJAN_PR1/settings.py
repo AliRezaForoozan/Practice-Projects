164c1e5a749a57dd3f894ad4c67ddfb40ab7fb26
alirf <designerafg@gmail.com>
2026-09-22 11:00:58 +0330

    fix static files config

--- MYJAN_PR1/MYJAN_PR1/settings.py
+++ MYJAN_PR1/MYJAN_PR1/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+#STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
