8b34282fef8261710ec2b6f8035f0e72a5afca48
alirf <designerafg@gmail.com>
2026-09-23 12:48:49 +0330

    New try to change sqllight to postgres db no2

--- MYJAN_PR1/Home/views.py
+++ MYJAN_PR1/Home/views.py
@@ -5,7 +5,7 @@ def home(request):
 
     allField = person.objects.all()
     frst = person.objects.first()
-    dic_persons = {"First_Name":"Ali","Last_Name":"Forouzan","allfield":allField,"first":frst}
+    dic_persons = {"First_Name":"AliR","Last_Name":"Forouzan","allfield":allField,"first":frst}
     return render(request,'index.html',context=dic_persons)
 
 # Create your views here.
