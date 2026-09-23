75a2b5632e60ea2b03b0b80ab04a09e0a452bcd0
alirf <designerafg@gmail.com>
2026-09-23 12:45:32 +0330

    New try to change sqllight to postgres db

--- MYJAN_PR1/MYJAN_PR1/settings.py
+++ MYJAN_PR1/MYJAN_PR1/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 from string import Template
-
+import dj_database_url
 from django.conf.global_settings import STATIC_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,10 +78,9 @@ WSGI_APPLICATION = 'MYJAN_PR1.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:Ali1211@localhost:5432/myjandb'
+    )
 }
 
 
